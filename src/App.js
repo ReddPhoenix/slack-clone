df1ada057d04075d9daeea8eaa9ffe364aa3c6cf
Walter Guerrero <wsguerrero@gmail.com>
2021-02-23 22:34:04 -0700

    created header component, added styled components dependency

--- src/App.js
+++ src/App.js
@@ -2,23 +2,33 @@ import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Chat from './components/Chat.jsx';
 import Login from './components/Login.jsx';
+import Header from './components/Header.jsx';
+import styled from 'styled-components';
+
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Switch>
-          <Route path='/room'>
-            <Chat />
-          </Route>
-          <Route path='/'>
-            <Login />
-          </Route>
-        </Switch>
+        <Container>
+          <Header />
+
+          <Switch>
+            <Route path='/room'>
+              <Chat />
+            </Route>
+            <Route path='/'>
+              <Login />
+            </Route>
+          </Switch>
+        </Container>
       </Router>
-      
     </div>
   );
 }
 
 export default App;
+
+const Container = styled.div`
+
+`
