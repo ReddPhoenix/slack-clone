b46e754f160391eb914570c152e06a13c7f0a65b
Walter Guerrero <wsguerrero@gmail.com>
2021-02-24 11:10:29 -0700

    added search bar and User to Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import AccessTimeIcon from '@material-ui/icons/AccessTime';
 import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 
 function Header() {
     return (
@@ -15,9 +16,14 @@ function Header() {
                 </SearchContainer>
                 <HelpOutlineIcon />
             </Main>
-            {/* <UserContainer>
-
-            </UserContainer> */}
+            <UserContainer>
+                <Name>
+                    Walter
+                </Name>
+                <UserImage>
+                    <AccountCircleIcon />
+                </UserImage>
+            </UserContainer>
         </Container>
     )
 }
@@ -25,16 +31,54 @@ function Header() {
 export default Header
 
 const Container = styled.div`
-
+    background: #350d36;
+    color: white;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
 `
+
 const Main = styled.div`
     display: flex;
 `
 
 const SearchContainer = styled.div`
+    min-width: 400px;
+    
+    `
+    
+    const Search = styled.div`
+    box-shadow: inset 0 0 0 1px rgb(104 74 104);    
+    width: 100%;
+    border-radius: 6px;
+    display: flex;
+    align-items: center;
+
+    input {
+        background-color: transparent;
+        border: none;
+        padding-left: 8px;
+        padding-right: 8px;
+        color: white;
+        padding-top: 4px;
+        padding-bottom: 4px;
+        }
+
+    input:focus {
+        outline: none;
+    }
+`
+
+const UserContainer = styled.div`
+    display: flex;
+    align-items: center;
+    padding-right: 16px;
+`
+
+const Name = styled.div`
 
 `
 
-const Search = styled.div`
+const UserImage = styled.div`
 
 `
